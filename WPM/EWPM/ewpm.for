d62fddfa1928fbebe32ad4179db72fa1ed8bfa60
Ale Mendez <alemdz.7@gmail.com>
2024-01-24 18:05:02 -0300

    include code for EWPM by Archubi & Arista (2017)

---
+++ WPM/EWPM/ewpm.for
@@ -0,0 +1,335 @@
+*
+*   This program computes stopping cross sections using the Extended Wave 
+*   Packet Model (UWPM) proposed by Archubi & Arista (2017)
+*
+      program EWPM
+      IMPLICIT REAL*8 (A-H,O-Z) 
+      character (24) systime
+      character(len=30) filename
+      character(len=:),allocatable:: str
+      parameter (NVMX=28)
+
+      common/CINDIV/VV
+      common/tolbck/TOLK,TOLW,TOLX
+      common/plas/enl,chi2,qbar,gamma
+      COMMON/CK/XK                    ! test
+
+      EXTERNAL fun1,GABQ
+      EXTERNAL fun2                ! test
+
+      dimension vp_au(NVMX)
+
+c.... Default velocity grid in a.u.
+      data vp_au/   0.1D0,   0.2D0,    0.3D0,    0.4D0,    0.6D0,
+     |              0.8D0,   1.0D0,    1.2D0,    1.4D0,    1.6D0,
+     |              1.8D0,   2.0D0,    2.4D0,    2.8D0,    3.0D0,
+     |              4.0D0,   4.5D0,    5.0D0,   6.32D0,   8.94D0,
+     |           10.954D0, 12.65D0, 14.142D0, 16.733D0,  20.00D0,
+     |            28.28D0, 47.00D0,  63.24D0/
+
+      pi = 3.1415926536
+      a0 = 0.529177210903E-8
+      xkB = 8.617333262E-5    ! eV/K 
+      conv_cm3_to_au = 0.08916
+      conv_au_to_eVcm2 = 0.76196d0
+      conv_au_to_A = 51.4194586
+      conv_au_to_eV = 27.2114
+      conv_eV_to_au = 1.d0/conv_au_to_eV
+      conv_eV_to_K = 11604.525
+      TOLK = 1.0e-3
+      TOLW = 1.0e-3
+      TOLX = 1.0e-3
+
+      enl = 0.d0
+      gamma = 1.d0
+c**********      READ DATA      ***********************
+      open(10,file='ewpm.inp',status='old')
+      read(10,*) vmin
+      read(10,*) vmax
+      read(10,*) number_electrons
+      read(10,*) q1
+      read(10,*) enl
+      read(10,*) gamma
+      close(10)
+
+      qbar = q1 * number_electrons**(1.d0/3.d0)
+      chi2 = 1.d0 / (pi*qbar)
+      wp_p2 = gamma * qbar**3 / sqrt(pi)
+      atomic_density_au = wp_p2 / (4.d0*pi*number_electrons)
+
+      write(*,1002)
+      write(*,*)'# qbar=',qbar
+      write(*,*)'# chi^2=',chi2
+      write(*,*)'# wp_p2=',wp_p2
+
+c.... f-sum rules -  Archubi & Arista (2017), Eq. (10)
+      XK  = 10.0
+      W1  = 1.E-5
+      W2  = 500.d0
+      TOL = 1.E-6
+      CALL GABQ(fun2,W1,W2,SUM1,TOL,IER)
+      theovalue = pi / 2.d0 * wp_p2
+
+      WRITE(*,1000) int(number_electrons),q1,enl,atomic_density_au
+      write(*,1005) chi2,qbar
+      write(*,1001) theovalue,SUM1
+
+c**********      OUTPUT FILE     ***************************
+      WRITE(*,*)'Enter the output file name'
+      READ(*,*) filename
+
+      str = trim(filename)//'_EWPM.dat'
+      open(7,file=str,status='unknown')
+      WRITE(7,1002) trim(filename)
+      write(7,1000) number_electrons,q1,enl,atomic_density_au
+      write(7,1005) chi2,qbar
+      WRITE(7,1003)
+      CLOSE(7)
+      
+1001  format(/"# Checking f-sum rule:"/,
+     |       "# Theo. value = ",f20.6,2x,"  Num. value = ",f12.6/)
+1002  format('# PROTON STOPPING POWER OF H on BOUND ELECTRONS',1x,
+     |       'USING THE EWPM'/,
+     |       '# Target:',1x,a)
+1000  format('# Number of electrons: ',i4/,
+     |       "# Kaneko's screening parameter: ",f12.3/,
+     |       "# Electron binding energy: ",f12.3,1x,"a.u."/,
+     |       '# Atomic density:',1x,1pe12.3,1x,'a.u.')
+1005  format("# Chi^2: ",f12.4/,
+     |       "# q_bar: ",f12.4)
+1003  FORMAT(/'# V(au)',3X,'SP(au/atom)',4X,'SP(10^-15eVcm2/atom)')
+
+***********************************************************************
+* COMPUTE THE STOPPING POWER OVER A GIVEN DISTRIBUTION OF VELOCITIES
+      do 200 iv=1,NVMX
+        v = vp_au(iv)
+        VV = v
+c.... add the minimum and maximum velocity given
+        if ((vp_au(iv+1).gt.vmin).and.(v.lt.vmin)) v = vmin
+        if ((v.gt.vmax).and.(vp_au(iv-1).lt.vmax)) v = vmax
+        if ((v.lt.vmin).or.(v.gt.vmax)) go to 200
+
+c.... define integration limits for k variable
+        EIK1 = 1.D-5
+c        EIK2 = 4.d0*(v+xkf)
+        if (v.lt.1) epsmax = 75.d0
+        if (v.ge.1) epsmax = 25.d0
+        EIK2 = epsmax*v
+
+c.... compute integral
+        call gabq(fun1,EIK1,EIK2,sum,TOLK,IER)
+        spp = 2/(pi*v**2) * sum
+        spp_conv = spp*conv_au_to_eVcm2/atomic_density_au
+
+c.... print results in terminal and output file
+        write(*,*) 'v=',v,'sp=', spp_conv
+        open(7,file=str,status='old',access='append')
+        write(7,1004) v, spp, spp_conv
+        close(7)
+200   continue
+
+1004  format(2X,F6.3,3X,1pE12.6,3x,e12.6,3x,e12.6)
+      stop
+      deallocate(str)
+      end program
+
+c********  FUNCTIONS AND SUBROUTINES **********************
+      FUNCTION FUN1(K)
+        IMPLICIT REAL*8 (A-H,O-Z)
+        REAL*8 K,KK
+c
+        COMMON /CINDIV/ V
+        COMMON /CK/ KK
+        EXTERNAL GABQ,fun2
+        KK  = K
+        W1  = 1.E-5
+        W2  = K*V
+        TOL = 1.E-6
+        CALL GABQ(fun2,W1,W2,SUM1,TOL,IER)
+        FUN1 = 1.d0/K* SUM1
+        RETURN
+       END
+*********************************************************
+      FUNCTION fun2(W)
+       IMPLICIT REAL*8 (A-H,O-Z)
+       REAL*8 K,W
+       COMMON/CK/K
+       EXTERNAL elfuwpm
+
+       CALL elfuwpm(w,k,ximelf)
+       fun2 = ximelf*W
+
+       RETURN
+       END
+*********************************************************
+      SUBROUTINE elfuwpm(w,xk,elf)
+       IMPLICIT REAL*8 (A-H,O-Z)
+       COMPLEX*16 df
+       common/plas/enl,chi2,xkf,gamma
+
+       pi = 3.1415926536
+       if (w.ge.enl) then
+        wg = sqrt(w**2 - enl**2)
+        ukw = wg / (xk*xkf)
+        zkw = xk / (2.d0*xkf)
+        e1 = gamma*chi2*(G(ukw+zkw) - G(ukw-zkw)) / (8.d0*zkw**3.d0)
+        e1 = 1.d0 + e1 
+        e2 = gamma * pi * chi2 / (8.d0*zkw**3.d0)
+        e2 = e2 * (exp(-(ukw - zkw)**2.d0) - exp(-(ukw + zkw)**2.d0))
+        df = dcmplx(e1, e2)
+        elf = dimag(-1.0d0 / df)
+c        elf = e2
+       else
+        elf = 0.d0
+       endif
+
+       return
+       END
+*********************************************************
+      function G(x)
+       implicit double precision (a-h,o-z)
+       external GABQ, gaint
+       common/tolbck/TOLK,TOLW,TOLX
+       common/gai/xx
+
+       pi = 3.1415926536
+
+       xx = x
+       eit1 = 1.d-5
+       eit2 = 1.0d0
+       call GABQ(gaint,eit1,eit2,sum2,tolx,ier)
+       G = 2.d0*sqrt(pi)*x*sum2
+
+       return
+       end
+*********************************************************
+      function gaint(t)
+       implicit double precision (a-h,o-z)
+       common/gai/x
+       xind = (t**2.d0 - 1.d0)*x**2.d0
+       gaint=dexp(xind)
+       return
+       end
+C  **************************************************************
+C                      SUBROUTINE GABQ
+C  **************************************************************
+      SUBROUTINE GABQ(FCT,XL,XU,SUM,TOL,IER)
+C
+C      THIS INTEGRATION SUBROUTINE APPLIES THE GAUSS METHOD WITH
+C   AN ADAPTIVE-BIPARTITION SCHEME.
+C      FCT IS THE (EXTERNAL) FUNCTION BEING INTEGRATED OVER THE
+C   INTERVAL (XL,XU). SUM IS THE RESULTANT VALUE OF THE INTEGRAL.
+C      TOL IS THE TOLERANCE, I.E. MAXIMUM RELATIVE ERROR REQUIRED
+C   ON THE COMPUTED VALUE (SUM). TOL SHOULD NOT EXCEED 1.0D-13.
+C      IER IS AN ERROR CONTROL PARAMETER; ITS OUTPUT VALUE IS
+C   IER=0 IF THE INTEGRATION ALGORITHM HAS BEEN ABLE TO GET THE
+C   REQUIRED ACCURACY AND IER=1 OTHERWISE.
+C
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+      DIMENSION X(10),W(10),S(128),SN(128),L(128),LN(128)
+C  ****  PRINTED OUTPUT OF PARTIAL RESULTS: SET IWR=1.
+      DATA IWR/0/
+C  ****  COEFFICIENTS FOR GAUSS 20-POINT INTEGRATION.
+      DATA NP,NP2,NP4/10,20,40/
+C  ****  ABSCISAS.
+      DATA X/7.6526521133497334D-02,2.2778585114164508D-01,
+     1       3.7370608871541956D-01,5.1086700195082710D-01,
+     2       6.3605368072651503D-01,7.4633190646015079D-01,
+     3       8.3911697182221882D-01,9.1223442825132591D-01,
+     4       9.6397192727791379D-01,9.9312859918509492D-01/
+C  ****  WEIGHTS.
+      DATA W/1.5275338713072585D-01,1.4917298647260375D-01,
+     1       1.4209610931838205D-01,1.3168863844917663D-01,
+     2       1.1819453196151842D-01,1.0193011981724044D-01,
+     3       8.3276741576704749D-02,6.2672048334109064D-02,
+     4       4.0601429800386941D-02,1.7614007139152118D-02/
+C  ****  CORRECTED TOLERANCE.
+      CTOL=DMAX1(TOL,1.0D-13)
+      PTOL=0.01D0*CTOL
+      H=XU-XL
+C
+      IF(IWR.EQ.1) THEN
+      WRITE(6,10)
+   10 FORMAT(///5X,'GAUSS ADAPTIVE-BIPARTITION QUADRATURE')
+      WRITE(6,11) XL,XU,TOL
+   11 FORMAT(/5X,'XL = ',1PD15.8,', XU = ',D15.8,', TOL =',
+     1 D8.1)
+      ENDIF
+      IER=0
+C  ****  GAUSS INTEGRATION FROM XL TO XU.
+      A=0.5D0*(XU-XL)
+      B=0.5D0*(XL+XU)
+      C=A*X(1)
+      D=W(1)*(FCT(B+C)+FCT(B-C))
+      DO 1 I1=2,NP
+      C=A*X(I1)
+      D=D+W(I1)*(FCT(B+C)+FCT(B-C))
+    1 continue 
+      SUM=D*A
+C  ****  ADAPTIVE BIPARTITION SCHEME.
+      ICALL=NP2
+      LH=1
+      S(1)=SUM
+      L(1)=1
+    2 HO=H
+      H=0.5D0*H
+      ASUM=SUM
+      LHN=0
+      DO 5 I=1,LH
+      K=L(I)
+      SI=S(I)
+      XA=XL+(K-1)*HO
+      XB=XA+H
+      XC=XA+HO
+      A=0.5D0*(XB-XA)
+      B=0.5D0*(XB+XA)
+      C=A*X(1)
+      D=W(1)*(FCT(B+C)+FCT(B-C))
+      DO 3 I2=2,NP
+      C=A*X(I2)
+      D=D+W(I2)*(FCT(B+C)+FCT(B-C))
+    3 continue 
+      S1=D*A
+      A=0.5D0*(XC-XB)
+      B=0.5D0*(XC+XB)
+      C=A*X(1)
+      D=W(1)*(FCT(B+C)+FCT(B-C))
+      DO 4 I3=2,NP
+      C=A*X(I3)
+      D=D+W(I3)*(FCT(B+C)+FCT(B-C))
+    4 continue 
+      S2=D*A
+      ICALL=ICALL+NP4
+      S12=S1+S2
+      SUM=SUM+S12-SI
+      IF(DABS(S12-SI).LT.DMAX1(PTOL*DABS(S12),1.0D-35)) GO TO 5
+      LHN=LHN+2
+      IF(LHN.GT.128.OR.ICALL.GT.9999) GO TO 8
+      SN(LHN)=S2
+      LN(LHN)=K+K
+      SN(LHN-1)=S1
+      LN(LHN-1)=LN(LHN)-1
+   5  CONTINUE
+      ERR=DABS(SUM-ASUM)/DMAX1(DABS(SUM),1.0D-35)
+      IF(IWR.EQ.1) WRITE(6,12) ICALL,SUM,ERR,LHN
+   12 FORMAT(5X,'N =',I5,', SUM =',1PD19.12,', ERR =',D8.1,
+     1 ', LH =',I3)
+      IF(ERR.GT.CTOL.AND.LHN.GT.0) GO TO 6
+      IF(IWR.EQ.1) WRITE(6,13)
+   13 FORMAT(5X,'END OF GAUSS-BIPARTITION PROCEDURE'///)
+      RETURN
+    6 LH=LHN
+      DO 7 I=1,LH
+      S(I)=SN(I)
+      L(I)=LN(I)
+    7 continue 
+      GO TO 2
+C  ****  WARNING (LOW ACCURACY) MESSAGE.
+    8 WRITE(6,14)
+   14 FORMAT(/5X,'*** LOW ACCURACY IN SUBROUTINE GABQ.')
+      WRITE(6,11) XL,XU,TOL
+      WRITE(6,15) SUM,ERR
+   15 FORMAT(5X,'SUM =',1PD19.12,', ERR =',D8.1//)
+      IER=1
+      RETURN
+      END
